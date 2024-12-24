4b212e543f59824b568433b919d43412ef313f9b
Vishnu Rangan <ruudvishnu10@gmail.com>
2024-12-24 20:23:02 +0530

    Updated tests.py

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
 from django.test import TestCase
 
 # Create your tests here.
-=======
-from django.test import TestCase
-
-# Create your tests here.
->>>>>>> e6b84dd (Commit#1 - Pushing project to Github)
